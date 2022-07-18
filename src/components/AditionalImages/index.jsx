36cb485cd06e08885841c0dc715a6a8a2ad25fd7
Marco Souza <marcoantonio99d@gmail.com>
2022-07-18 15:33:24 -0300

    product detail

--- src/components/AditionalImages/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './style.css';
-
-export default function AditionalImages(props) {
-    const { src, alt } = props;
-  return (
-      <img className="aditional-image" src={ src } alt={ alt } />
-  )
-}
---
+++ src/components/ProductSlide/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+export default function ProductSlide(props) {
+  const { fadeImages } = props;
+  return (
+    <div className="slide-container">
+        <Fade>
+        {fadeImages.map((fadeImage, index) => (
+          <div className="each-fade" key={index}>
+            <div className="image-container">
+              <img src={fadeImage.img} />
+            </div>
+          </div>
+        ))}
+      </Fade>
+      </div>
+  )
+}
