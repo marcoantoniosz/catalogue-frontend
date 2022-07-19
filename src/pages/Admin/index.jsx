6110767fb9b7dfe21ea1d270cd2d7850422b8dcf
Marco <marcoantonio99d@gmail.com>
2022-07-19 19:06:12 -0300

    Tela de login

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Dashboard from '../../components/Dashboard/Dashboard';
 
 export default function Admin() {
   return (
-    <div>Admin</div>
+    <Dashboard />
   )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,21 +1,108 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
-import Marca from '../../images/marca.png';
-import './style.css';
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
 
-
-export default function Admin() {
-	const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
-  
+function Copyright(props) {
   return (
-    <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-				<img className="brand" src={ Marca } alt="marca" />
-				<input type="text" placeholder="&#xf007; Usuário" {...register("user" , { required:true })} />
-				<input type="password" placeholder="&#xf023; Senha" {...register("password", { required: true })} />
-				<button type="submit">Login</button>
-    	</form>
-    </div>
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.m3soft.com.br/">
+        M3Soft
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+
+export default function SignIn() {
+
+  const navigate = useNavigate();
+
+  const schema = yup.object({
+    user: yup.string().required("O nome de usuário é obrigatório").matches("M3user", "Nome de usuário inválido"),
+    password: yup.string().required("A senha é obrigatória").matches("M3pass", "Senha inválida"),
+  }).required();
+
+
+  const { register, handleSubmit, formState:{ errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = () => (navigate("/admin"));
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#F2055C' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Painel de Controle
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="user"
+              label="Usuário"
+              name="user"
+              autoComplete="user"
+              autoFocus
+              {...register("user")}
+              error={!!errors?.user}
+              helperText={errors?.user ? errors.user.message : null}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              {...register("password")}
+              error={!!errors?.password}
+              helperText={errors?.password ? errors.password.message : null}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, bgcolor: '#2405F2' }}
+            >
+              Entrar
+            </Button> 
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
